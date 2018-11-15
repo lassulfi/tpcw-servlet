dc21abfc08cc19a2d23d280434d31dd1d4c1d211
esoft35 - Luis Daniel Assulfi <esoft35@students.ic.unicamp.br>
2018-11-15 16:08:49 -0200

    C02T03 - Criadas classes com a estrutura de dados de avaliação

---
+++ src/main/java/Evaluation.java
@@ -0,0 +1,56 @@
+
+/**
+ * C02T03
+ * 
+ * Classe de avaliação de dados
+ * @author esoft35
+ *
+ */
+public class Evaluation {
+	
+	private int id;
+	private Customer customer;
+	private Book book;
+	private int rate;
+	
+	public Evaluation(int id, Customer customer, Book book, int rate) {
+		this.customer = customer;
+		this.book = book;
+		this.rate = rate;
+	}
+	
+		
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+	
+	public Customer getCustomer() {
+		return customer;
+	}
+
+	public Book getBook() {
+		return book;
+	}
+
+	public void setBook(Book book) {
+		this.book = book;
+	}
+
+	public int getRate() {
+		return rate;
+	}
+
+	public void setRate(int rate) {
+		this.rate = rate;
+	}	
+
+}
