bff5bf3dafbe8fdb314aeb84b115e118ed6865f9
esoft12-BrunoEmilio <brunno16@gmail.com>
2018-11-15 16:26:00 -0200

    Implementação da tarefa C02T05 - Bruno Emilio e Felipe Pires

---
+++ src/main/java/Recommender.java
@@ -0,0 +1,53 @@
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import org.apache.mahout.cf.taste.common.TasteException;
+import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
+import org.apache.mahout.cf.taste.impl.neighborhood.ThresholdUserNeighborhood;
+import org.apache.mahout.cf.taste.impl.recommender.GenericUserBasedRecommender;
+import org.apache.mahout.cf.taste.impl.similarity.PearsonCorrelationSimilarity;
+
+import org.apache.mahout.cf.taste.model.DataModel;
+import org.apache.mahout.cf.taste.neighborhood.UserNeighborhood;
+
+import org.apache.mahout.cf.taste.recommender.RecommendedItem;
+import org.apache.mahout.cf.taste.recommender.UserBasedRecommender;
+
+import org.apache.mahout.cf.taste.similarity.UserSimilarity;
+
+/**
+ * @author esoft12 e esoft25
+ * Esta classe é a implementação da tarefa C02T05
+ */
+public class Recommender {
+	
+	private File file;
+	
+	public Recommender(File _file) {		
+	    file = _file;
+	}
+	
+	public List<RecommendedItem> GetRecommender() throws TasteException, IOException
+	{
+		 //Creating data model
+	    DataModel datamodel = new FileDataModel(file); //data
+	 
+	    //Creating UserSimilarity object.
+	    UserSimilarity usersimilarity = new PearsonCorrelationSimilarity(datamodel);
+	 
+	    //Creating UserNeighbourHHood object.
+	    UserNeighborhood userneighborhood = new ThresholdUserNeighborhood(3.0, usersimilarity, datamodel);
+	 
+	    //Create UserRecomender
+	    UserBasedRecommender recommender = new GenericUserBasedRecommender(datamodel, userneighborhood, usersimilarity);
+	   
+	    return  recommender.recommend(2, 3);				  
+	}	
+	
+	public void SetFile(File _file)
+	{
+		 file = _file;
+	}
+}
